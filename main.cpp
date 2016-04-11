1cbe884ad043edad487e0d4f3399687bba401d1b
LeoBrilliant <LeoBrilliant@gmail.com>
2016-04-11 13:15:38 +0800

    Hello OpenTS
    Signed-off-by: LeoBrilliant <LeoBrilliant@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2016Äê4ÔÂ11ÈÕ
+ *      Author: user
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello OpenTS!" << endl;
+	return 0;
+}
+
+
